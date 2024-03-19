f3e28027202ea4f98932307dd71e5e8f1daa1a53
pleasingcat <157977855+pleasingcat@users.noreply.github.com>
2024-03-19 17:12:55 +0000

    rpc

--- app/main.js
+++ app/main.js
@@ -1,4 +1,13 @@
 const { app, BrowserWindow } = require('electron');
+
+
+let client = new (require('discord-rpc-revamp').Client)();
+
+
+client.connect({ clientId: '1218201961296564297' }).catch(console.error);
+
+
+
 const url = 'https://kour.io';
 
 const createWindow = () => {
@@ -7,11 +16,27 @@ const createWindow = () => {
     height: 1080,
     titleBarStyle: 'hidden',
   });
-    win.removeMenu();
-    win.maximize();
-    win.once('focus', () => win.flashFrame(false));
+  win.removeMenu();
+  win.maximize();
+  win.once('focus', () => win.flashFrame(false));
   win.flashFrame(true);
-    win.loadURL(url);
+  win.loadURL(url);
+
+
+  client.on('ready', _ => {
+    client.setActivity({
+      details: 'EKC-Client',
+      largeImageKey: 'kour',
+      startTimestamp: Date.now()
+    }).then(_ => {
+      console.log('rpc set');
+    });
+
+    client.subscribe('ACTIVITY_JOIN');
+    client.subscribe('ACTIVITY_JOIN_REQUEST');
+
+    client.on('ACTIVITY_JOIN', data => {});
+  });
 };
 
 app.whenReady().then(() => {
