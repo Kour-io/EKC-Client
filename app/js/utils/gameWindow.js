ef88006e9c6d8886cfb3fe223f22a55fe5fb4d5d
Error430 <noahdunnagan@protonmail.com>
2024-03-20 08:44:36 -0400

    Messing with store features

--- app/js/utils/gameWindow.js
+++ app/js/utils/gameWindow.js
@@ -3,8 +3,17 @@ const client = new (require('discord-rpc-revamp').Client)();
 const platformType = require('os').platform();
 const path = require('node:path');
 const rpc = require('./rpc.js');
-const wombo = require('../../private.js');
-
+let wombo;
+try {
+    wombo = require('../../private.js');
+} 
+catch (error) {
+    console.error('Error loading private.js:', error);
+    wombo = {};
+}
+const Store = require('electron-store');
+const store = new Store();
+const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0';
 const windows = {
     game: null,
 };
@@ -22,6 +31,7 @@ exports.gameWindow = class {
             titleBarStyle: 'hidden',
             webPreferences: {
                 preload: path.join(__dirname, '../preload/game.js'),
+                userAgent: userAgent,
             },
         });
         
@@ -31,6 +41,21 @@ exports.gameWindow = class {
             win.flashFrame(true);
             win.loadURL((!url) ? 'https://kour.io' : url);
             console.log('[MAIN]'.bgGreen, 'Page loaded'.green);
+            
+            // eslint-disable-next-line max-statements-per-line
+            if (clipboard.readText() === wombo.gspinny) { store.set('isgspinny', 'enabled'); }
+            if (store.get('isgspinny') === 'enabled') {
+                win.webContents.executeJavaScript(`
+                const gifImg = document.createElement('img');
+                gifImg.src = 'https://i.imgur.com/FEc60NL.gif';
+                gifImg.style.position = 'absolute';
+                gifImg.style.bottom = '300px';
+                gifImg.style.right = '10px';
+                gifImg.style.width = '200px';
+                gifImg.style.height = '200px';
+                document.body.appendChild(gifImg);
+                `);
+            }
         
         win.once('ready-to-show', () => {
             console.log('[MAIN]'.bgGreen, 'Web Content Ready To Show'.green);
