bdb5765450f7fc121c8682bcd33d6d7b8dd20761
Error430 <noahdunnagan@protonmail.com>
2024-03-20 11:53:30 -0400

    :D

--- app/js/utils/gameWindow.js
+++ app/js/utils/gameWindow.js
@@ -4,14 +4,20 @@ const platformType = require('os').platform();
 const fs = require('fs');
 const path = require('path');
 const rpc = require('./rpc.js');
-let wombo;
+let wombo = null; // Initialize wombo to null
+
 try {
     wombo = require('../../private.js');
 } 
 catch (error) {
-    console.error('Error loading private.js:', error);
-    wombo = {};
+    wombo = null; // Set wombo to null if private.js cannot be loaded
+    console.log('wombo set to', wombo);
 }
+
+process.on('unhandledRejection', (error) => {
+    console.error('Unhandled Promise Rejection:', error);
+});
+
 const Store = require('electron-store');
 const store = new Store();
 const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0';
@@ -35,18 +41,19 @@ exports.gameWindow = class {
                 userAgent: userAgent,
             },
         });
-        
-            win.removeMenu();
-            win.maximize();
-            win.once('focus', () => win.flashFrame(false));
-            win.flashFrame(true);
-            win.loadURL((!url) ? 'https://kour.io' : url);
-            console.log('[MAIN]'.bgGreen, 'Page loaded'.green);
-            
-            // eslint-disable-next-line max-statements-per-line
+
+        win.removeMenu();
+        win.maximize();
+        win.once('focus', () => win.flashFrame(false));
+        win.flashFrame(true);
+        win.loadURL((!url) ? 'https://kour.io' : url);
+        console.log('[MAIN]'.bgGreen, 'Page loaded'.green);
+
+        if (wombo) {
             if (clipboard.readText() === wombo.gspinny) { store.set('isgspinny', 'enabled'); }
-            if (store.get('isgspinny') === 'enabled') {
-                win.webContents.executeJavaScript(`
+        }
+        if (store.get('isgspinny') === 'enabled') {
+            win.webContents.executeJavaScript(`
                 const gifImg = document.createElement('img');
                 gifImg.src = 'https://i.imgur.com/FEc60NL.gif';
                 gifImg.style.position = 'absolute';
@@ -55,17 +62,26 @@ exports.gameWindow = class {
                 gifImg.style.width = '200px';
                 gifImg.style.height = '200px';
                 document.body.appendChild(gifImg);
-                `);
-            }
-        
+            `);
+        }
+
         win.once('ready-to-show', () => {
             console.log('[MAIN]'.bgGreen, 'Web Content Ready To Show'.green);
-            globalShortcut.register(wombo.wombo, () => {
-                win.webContents.openDevTools({ mode: 'detach' });
+            if (wombo) { // Check if wombo is defined before using it
+                globalShortcut.register(wombo.wombo, () => {
+                    win.webContents.openDevTools({ mode: 'detach' });
+                });
+            }
+            globalShortcut.register('F11', () => {
+                const brWin = BrowserWindow.getFocusedWindow();
+                if (brWin) {
+                    const isFullScreen = brWin.isFullScreen();
+                    store.set('Fullscreen', !isFullScreen);
+                    brWin.setFullScreen(!isFullScreen);
+                }
             });
         });
+
         const rpcInstance = new rpc();
-        
     }
 };
-
