4aeabfe099a36c2752f3e78f763e9cf0917ec635
Error430 <noahdunnagan@protonmail.com>
2024-04-03 13:31:51 -0400

    Fixed bug from new kour update
    :D

--- app/js/utils/gameWindow.js
+++ app/js/utils/gameWindow.js
@@ -1,83 +1,45 @@
-const { app, BrowserWindow, globalShortcut, clipboard, ipcRenderer } = require('electron');
+/* eslint-disable quotes */
+/* eslint-disable max-statements-per-line */
+const { app, BrowserWindow, globalShortcut, clipboard } = require('electron');
 const log = require('electron-log');
-const colors = require('colors');
-const platformType = require('os').platform();
-const fs = require('fs');
+const { registerShortcut, unregisterAllShortcuts } = require('./shortcuts.js');
 const path = require('path');
 const rpc = require('./rpc.js');
 const Toastify = require('toastify-js');
-const { registerShortcut, unregisterAllShortcuts } = require('./shortcuts.js');
-let wombo = null; // Initialize wombo to null
+const Store = require('electron-store');
+const store = new Store();
+const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0';
+
+let wombo = null;
 try {
     wombo = require('../../private.js');
 } 
 catch (error) {
-    wombo = null; // Set wombo to null if private.js cannot be loaded
-    console.log('wombo set to', wombo);
+    wombo = null;
 }
 
-process.on('unhandledRejection', (error) => {
-    console.error('Unhandled Promise Rejection:', error);
-});
+process.on('unhandledRejection', (error) => console.error('Unhandled Promise Rejection:', error));
 
-const Store = require('electron-store');
-const Logger = require('electron-log');
-const { config } = require('process');
-const store = new Store();
-const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0';
-const windows = {
-    game: null,
-    launcher: null,
-};
-
-
-exports.launchGame = (url = null) => {
-    windows.game = new this.gameWindow(url);
-    return windows.game;
-};
-
-exports.openCui = () => {
-    return new exports.crossWindow();
-};
-
-exports.startLauncher = () => {
-    windows.launcher = new this.launcherWindow();
-    return windows.launcher;
-};
-
-exports.closeLauncher = () => {
-    if (windows.launcher && !windows.launcher.win.isDestroyed()) {
-        windows.launcher.win.close();
-        windows.launcher = null; // Reset launcher reference
-    }
-};
-
+const windows = { game: null, launcher: null };
+const launchGame = (url = null) => { windows.game = new gameWindow(url); return windows.game; };
+const openCui = () => new crossWindow();
+const startLauncher = () => { windows.launcher = new launcherWindow(); return windows.launcher; };
+const closeLauncher = () => { if (windows.launcher && !windows.launcher.win.isDestroyed()) { windows.launcher.win.close(); windows.launcher = null; } };
 let cuiWindow = null;
 
 const launchCui = () => {
-    if (cuiWindow && !cuiWindow.win.isDestroyed()) { // Check if the window is already open
-        cuiWindow.focus(); // Bring the existing window to focus
-    } 
-    else {
-        cuiWindow = exports.openCui(); // Create a new window if it's not already open
-    }
+    if (cuiWindow && !cuiWindow.win.isDestroyed()) cuiWindow.focus();
+    else cuiWindow = openCui();
     return cuiWindow;
 };
 
-
-exports.gameWindow = class {
+class gameWindow {
     constructor(url) {
         const win = new BrowserWindow({
             width: 1920,
             height: 1080,
             titleBarStyle: 'hidden',
-            webPreferences: {
-                preload: path.join(__dirname, '../preload/game.js'),
-                userAgent: userAgent,
-                contextIsolation: false,
-                nodeIntegration: true,
-                devTools: true,
-            },
+            webPreferences: { preload: path.join(__dirname, '../preload/game.js'), userAgent, contextIsolation: false, nodeIntegration: false, resizeable: false },
         });
 
         win.removeMenu();
@@ -85,54 +47,19 @@ exports.gameWindow = class {
         win.once('focus', () => win.flashFrame(false));
         win.flashFrame(true);
         win.loadURL((!url) ? 'https://kour.io' : url);
-        console.log('[MAIN]'.bgGreen, 'Page loaded'.green);
-        // Store the fullscreen state when it changes
-        win.on('enter-full-screen', () => {
-            store.set('Fullscreen', true);
-        });
 
-        win.on('leave-full-screen', () => {
-            store.set('Fullscreen', false);
-        });
+        win.on('enter-full-screen', () => store.set('Fullscreen', true));
+        win.on('leave-full-screen', () => store.set('Fullscreen', false));
         win.setFullScreen(store.get('Fullscreen'));
 
-
         const updateCrosshair = () => {
-            log.info('Updating crosshair...');
             const { sizeX, sizeY, url } = loadCrosshairPrefs();
-            log.info('Crosshair preferences:', sizeX, sizeY, url);
             win.webContents.executeJavaScript(`
-                console.log('Executing JavaScript to update crosshair...');
                 const crosshairImage = document.getElementById('crosshairImage');
-                console.log('Crosshair image:', crosshairImage);
-                if (!crosshairImage) {
-                    const img = new Image();
-                    img.style.position = 'absolute';
-                    img.id = 'crosshairImage';
-                    img.style.top = '50%';
-                    img.style.left = '50%';
-                    img.style.userSelect = 'none';
-                    img.style.pointerEvents = 'none';
-                    img.style.transform = 'translate(-50%, -50%)';
-                    img.onload = function() {
-                        this.width = ${sizeX};
-                        this.height = ${sizeY};
-                    };
-                    img.src = "${url}"; // Set the src attribute to the URL
-                    console.log('Appended new crosshair image:', img);
-                    document.body.appendChild(img);
-                } else {
-                    crosshairImage.src = "${url}";
-                    crosshairImage.onload = function() {
-                        this.width = ${sizeX};
-                        this.height = ${sizeY};
-                    };
-                    console.log('Updated existing crosshair image:', crosshairImage);
-                }
+                if (!crosshairImage) { const img = new Image(); img.style.position = 'absolute'; img.id = 'crosshairImage'; img.style.top = '50%'; img.style.left = '50%'; img.style.userSelect = 'none'; img.style.pointerEvents = 'none'; img.style.transform = 'translate(-50%, -50%)'; img.onload = function() { this.width = ${sizeX}; this.height = ${sizeY}; }; img.src = "${url}"; document.body.appendChild(img); } else { crosshairImage.src = "${url}"; crosshairImage.onload = function() { this.width = ${sizeX}; this.height = ${sizeY}; }; }
             `);
         };
-        
-    
+
         const loadCrosshairPrefs = () => {
             const sizeX = store.get('xScale') || '70';
             const sizeY = store.get('yScale') || '70';
@@ -140,128 +67,55 @@ exports.gameWindow = class {
             return { sizeX, sizeY, url };
         };
 
-
         win.once('ready-to-show', () => {
-            console.log('[MAIN]'.bgGreen, 'Web Content Ready To Show'.green);
             updateCrosshair(); 
-
-            if (wombo) { // Check if wombo is defined before using it
-                globalShortcut.register(wombo.wombo, () => {
-                    win.webContents.openDevTools({ mode: 'detach' });
-                });
-            }
-
-            registerShortcut('F11', () => {
-                const brWin = BrowserWindow.getFocusedWindow();
-                if (brWin) {
-                    const isFullScreen = brWin.isFullScreen();
-                    brWin.setFullScreen(!isFullScreen);
-                }
-            });
-
-            registerShortcut('Escape', () => {
-                const focusedWindow = BrowserWindow.getFocusedWindow();
-                if (focusedWindow) {
-                    focusedWindow.webContents.executeJavaScript(`
-                        if (document.pointerLockElement) {
-                            document.exitPointerLock();
-                        }
-                    `);
-                }
-            });
-
-            registerShortcut('F5', () => {
-                win.loadURL('https://kour.io');
-                
-            });
-
-            registerShortcut('F6', () => {
-                const clipboardText = clipboard.readText();
-                const focusedWindow = BrowserWindow.getFocusedWindow();
-                
-                if (focusedWindow && clipboardText.startsWith('https://kour.io')) {
-                    focusedWindow.loadURL(clipboardText);
-                }
-            });
-            
-
-            registerShortcut('CmdOrCtrl+Alt+V', () => {
-                launchCui();
-            });
-
-            registerShortcut('Alt+1', () => {
-                win.loadURL('https://kour.io/op');
-                
-            });
-
-            for (let i = 2; i <= 9; i++) {
-                registerShortcut(`Alt+${i}`, () => {
-                    win.loadURL(`https://kour.io/op${i}`);
-                    
-                });
-            }
-            
+            if (wombo) globalShortcut.register(wombo.wombo, () => win.webContents.openDevTools({ mode: 'detach' }));
+            registerShortcut('F11', () => BrowserWindow.getFocusedWindow()?.setFullScreen(!BrowserWindow.getFocusedWindow()?.isFullScreen()));
+            registerShortcut('Escape', () => BrowserWindow.getFocusedWindow()?.webContents.executeJavaScript(`if (document.pointerLockElement) document.exitPointerLock();`));
+            registerShortcut('F5', () => win.loadURL('https://kour.io'));
+            registerShortcut('F6', () => { const clipboardText = clipboard.readText(); if (clipboardText.startsWith('https://kour.io')) BrowserWindow.getFocusedWindow()?.loadURL(clipboardText); });
+            registerShortcut('CmdOrCtrl+Alt+V', () => launchCui());
+            registerShortcut('Alt+1', () => win.loadURL('https://kour.io/op'));
+            for (let i = 2; i <= 9; i++) registerShortcut(`Alt+${i}`, () => win.loadURL(`https://kour.io/op${i}`));
         });
 
         const rpcInstance = new rpc();
-        win.webContents.on('did-finish-load', () => {
-            updateCrosshair();
-        });
+        win.webContents.on('did-finish-load', () => updateCrosshair());
     }
-};
+}
 
-exports.crossWindow = class {
+class crossWindow {
     constructor() {
         this.win = new BrowserWindow({
             width: 550,
             height: 200,
             y: 150,
             titleBarStyle: 'hidden',
-            webPreferences: {
-                preload: path.join(__dirname, '../preload/cross.js'),
-                userAgent: userAgent,
-                contextIsolation: false,
-                nodeIntegration: true,
-            },
+            webPreferences: { preload: path.join(__dirname, '../preload/cross.js'), userAgent, contextIsolation: false, nodeIntegration: true },
         });
         this.win.removeMenu();
         this.win.once('focus', () => this.win.flashFrame(false));
         this.win.flashFrame(true);
         this.win.loadFile(path.join(__dirname, '../../html/crossWindow.html'));
-
-        this.win.webContents.on('did-finish-load', () => {
-            if (!cuiWindow) {
-                cuiWindow = new this.crossWindow();
-            }
-        });
+        this.win.webContents.on('did-finish-load', () => { if (!cuiWindow) cuiWindow = new crossWindow(); });
     }
+    focus() { if (this.win && !this.win.isDestroyed() && !this.win.isFocused()) this.win.focus(); }
+}
 
-    focus() {
-        if (this.win && !this.win.isDestroyed() && !this.win.isFocused()) {
-            this.win.focus();
-        }
-    }
-};
-
-exports.launcherWindow = class {
+class launcherWindow {
     constructor() {
         this.win = new BrowserWindow({
             width: 1920,
             height: 1080,
             titleBarStyle: 'hidden',
             frame: false,
-            webPreferences: {
-                preload: path.join(__dirname, '../preload/launcher.js'),
-                userAgent: userAgent,
-                contextIsolation: false,
-                nodeIntegration: true,
-            },
+            webPreferences: { preload: path.join(__dirname, '../preload/launcher.js'), userAgent, contextIsolation: false, nodeIntegration: true },
         });
         this.win.removeMenu();
         this.win.once('focus', () => this.win.flashFrame(false));
         this.win.flashFrame(true);
         this.win.loadFile(path.join(__dirname, '../../html/launcher.html'));
-
     }
+}
 
-};
+module.exports = { launchGame, openCui, startLauncher, closeLauncher };
